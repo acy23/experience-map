7a738f4ba95deae417c1aa34cddbc611089ee382
efe <efeeozcann@gmail.com>
2024-01-27 00:44:09 +0300

    .

---
+++ getPersonalCountByCityId.php
@@ -0,0 +1,36 @@
+<?php
+    include('database.php');
+
+    if (isset($_GET['cityId'])) {
+        $plakakodu = $_GET['cityId'];
+
+        try {
+            $query = "SELECT COUNT(*) from employees where city_id = $cityId";
+
+            $result = $mysqli->query($query);
+
+            // Check if the query was successful
+            if ($result) {
+                // Fetch data from the result
+                $data = $result->fetch_all(MYSQLI_ASSOC);
+
+                // Return the data as JSON
+                header('Content-Type: application/json');
+                echo json_encode($data);
+            } else {
+                // Handle query error
+                echo json_encode(['error' => $mysqli->error]);
+            }
+        } catch (Exception $e) {
+            // Handle other types of exceptions if necessary
+            echo json_encode(['error' => $e->getMessage()]);
+        } finally {
+            // Close the connection in the finally block
+            $mysqli->close();
+        }
+    } else {
+        echo "Invalid request";
+    }
+
+
+?>
